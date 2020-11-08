169c22f910ca96aa1a4834df6451cdde916fcd93
MingliZhang <zml2416154671@gmail.com>
2020-11-08 17:49:25 -0500

    links between login and signup established.

---
+++ indexScript.js
@@ -0,0 +1,13 @@
+export const handleSignUpButtonPress = function () {
+
+}
+
+$(function () {
+  const $form = $('#login-form');
+  const $message = $('#message');
+
+  const $signUp = $(".signUp");
+  
+});
+
+
