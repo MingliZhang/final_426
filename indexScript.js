d4f333733bb43349a614dce0450b09364e200ec7
MingliZhang <zml2416154671@gmail.com>
2020-11-10 00:34:16 -0500

    need some help on the login button

--- indexScript.js
+++ indexScript.js
@@ -1,8 +1,13 @@
-export const handelLoginButtonPress = function(){
+const handelLoginButtonPress = function(event){
+    // If I comment the below code out, the auto detection of email and password field goes off. But if I include it, the page will refresh everytime the button is pushed!
+    // event.preventDefault();
+    // event.stopPropagation();
     let email = $("#email").val();
     let password = $("#password").val();
+    console.log("The email input is : " + email);
+    console.log("The password input is : "+ password);
 }
 
 $(function () {
-    $("#loginButton").on("click",)
+    $("#loginButton").on("click",handelLoginButtonPress)
 });
