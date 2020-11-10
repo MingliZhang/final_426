c1e854ea75866f08bf4bafceaba110bdad5f1f96
MingliZhang <zml2416154671@gmail.com>
2020-11-10 14:41:24 -0500

    signup script ready, need backend

---
+++ signUp/signUp.js
@@ -0,0 +1,42 @@
+function ValidateEmail(mail) {
+    let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (mail.match(mailformat)){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+const handelLoginButtonPress = function (event) {
+    // If I comment the below code out, the auto detection of email and password field goes off. But if I include it, the page will refresh everytime the button is pushed!
+    event.preventDefault();
+    event.stopPropagation();
+    let email = $("#email").val();
+    let password1 = $("#password1").val();
+    let password2 = $("#password2").val();
+
+    // need to make a call to the backend and find the user id in this case;
+    const $message = $("#message");
+    if (email.length !== 0 && password1.length !== 0 && ValidateEmail(email) && password2.length !== 0 && password1 === password2 ){
+        window.location.href = "../test.html?email=" + email + "&password=" + password1;
+    }else{
+        let message = "";
+        if(email.length ===0 || password1.length === 0 || password2.length === 0){
+            message = "Please provide all the needed fields above!";
+        }else if(! ValidateEmail(email)){
+            message = "Please provide a valid email address!";
+        }else if(password1 !== password2){
+            message = "The two password provided does not match each other!";
+        }else{
+            message = "This case is not covered!";
+        }
+        $message.empty();
+        $message.append(`<p style="font-weight: bold; color:red">${message}</p>`);
+    }
+}
+
+
+
+$(function () {
+    $("#signUpButton").on("click", handelLoginButtonPress);
+});
