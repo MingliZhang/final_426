4b8e0acc19f62fc38d6d27f56ec2867b1b011443
Eva Hu <huyinuo@live.unc.edu>
2020-11-16 23:41:22 +0800

    Update links

--- main_interface/navlinks.js
+++ main_interface/navlinks.js
@@ -1,7 +1,7 @@
 $(function(){
     $('#homeicon a').attr("href", "main_interface.html")
     $('#gameicon a').attr("href", "../game/index.html")
-    $('#inboxicon a').attr("href", "#")
+    $('#inboxicon a').attr("href", "../postbox/index.html")
     $('#chaticon a').attr("href", "#")
     $('#usericon a').attr("href", "#")
 })
--- postbox/navlinks.js
+++ postbox/navlinks.js
@@ -1,7 +1,7 @@
 $(function(){
     $('#homeicon a').attr("href", "../main_interface/main_interface.html")
-    $('#gameicon a').attr("href", "index.html")
-    $('#inboxicon a').attr("href", "../postbox/index.html")
+    $('#gameicon a').attr("href", "../game/index.html")
+    $('#inboxicon a').attr("href", "index.html")
     $('#chaticon a').attr("href", "#")
     $('#usericon a').attr("href", "#")
     $('.icons a').css("color", "#f3ECDB")
