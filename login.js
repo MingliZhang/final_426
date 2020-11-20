0a45ac8b94e6cf7c60df7eceea294bd43dcbf4e2
MingliZhang <zml2416154671@gmail.com>
2020-11-20 13:27:05 -0500

    The login page is reworked and just need to add the part that tells the rest of the webpages which user loged in.

--- login.js
+++ login.js
@@ -1,47 +1,49 @@
 function ValidateEmail(mail) {
-    let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if (mail.match(mailformat)){
-        return true;
-    }else {
-        return false;
-    }
+  let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (mail.match(mailformat)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 const handelLoginButtonPress = async function (event) {
-    // If I comment the below code out, the auto detection of email and password field goes off. But if I include it, the page will refresh everytime the button is pushed!
-    event.preventDefault();
-    event.stopPropagation();
-    let email = $("#email").val();
-    let password = $("#password").val();
+  // If I comment the below code out, the auto detection of email and password field goes off. But if I include it, the page will refresh everytime the button is pushed!
+  event.preventDefault();
+  event.stopPropagation();
+  let email = $("#email").val();
+  let password = $("#password").val();
 
-    // need to make a call to the backend and find the user id in this case;
-    const $message = $("#message");
-    
-    const result = await axios({
-        method: 'get',
-        url: `/api/users/${email}`
-    })
+  // need to make a call to the backend and find the user id in this case;
+  const $message = $("#message");
 
-    if (result.data.length!==0&&result.data[0].password===password&&email.length !== 0 && password.length !== 0 && ValidateEmail(email)) {
+  let message = "";
+  if (email.length === 0 || password.length === 0) {
+    message = "Please provide all the needed fields!";
+  } else if (!ValidateEmail(email)) {
+    message = "Please provide a valid email address!!";
+  } else {
+    try {
+      const result = await axios({
+        method: "get",
+        url: `https://us-central1-comp426-firebase.cloudfunctions.net/users/email/${email}`,
+      });
+      let data = result.data;
+      if (data.password !== password) {
+        message = "The password provided seems to be Incorrect!";
+      } else {
+        console.log("jump to the main page");
         window.location.href = "./main_interface/main_interface.html";
-    }else {
-        let message = "";
-        if(email.length === 0 || password.length === 0){
-            message = "Please provide all the needed fields above!";
-        }else if (!ValidateEmail(email)){
-            message = "Please provide a valid email address!!";
-        } else if(result.data.length===0){
-            message = "Email not found";
-        } else {
-            message = "Incorrect password";
-        }
-        $message.empty();
-        $message.append(`<p style="font-weight: bold; color:red">${message}</p>`);
+        // This is where the jump to the main page is at, at this point, the email and password provided match what we have in the database. The data of the user is in the data filed.
+      }
+    } catch (err) {
+      message = "This email has not been registered yet!";
     }
-}
-
-
+  }
+  $message.empty();
+  $message.append(`<p style="font-weight: bold; color:red">${message}</p>`);
+};
 
 $(function () {
-    $("#loginButton").on("click", handelLoginButtonPress);
+  $("#loginButton").on("click", handelLoginButtonPress);
 });
