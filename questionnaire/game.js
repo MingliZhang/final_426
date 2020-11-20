00b291067e5f6eced0247e0de359929cecb7339c
lkh9908 <46586250+lkh9908@users.noreply.github.com>
2020-11-20 00:05:03 -0500

    basics of questionnaire done

--- questionnaire/game.js
+++ questionnaire/game.js
@@ -6,6 +6,9 @@ const user = getUser()
 const questions = getQuestions()
 const questionDisplay = document.getElementById("questionBody")
 const rateDisplay = document.getElementById("rateBody")
+const body = document.getElementById("body")
+const submitDisplay = document.getElementById("submitDisplay")
+
 const startButton = document.getElementById("startButton")
 const matchButton = document.getElementById("matchButton")
 
@@ -15,25 +18,40 @@ let rateBox
 let rateBoxes = []
 let rate
 let rates = []
-let finalScore = 0
+
 let scores = new Array(10).fill(0)
 let currentPage = 0
 
 let clicked = new Array(50).fill(false);
 
+// final scores
+let finalScore = {
+    extraversion : 0,
+    agreeableness : 0,
+    conscientiousness : 0,
+    emotional_stability : 0,
+    intellect : 0
+}
+
+
 
 function initialte() {
+    document.getElementById("scoreBoard").style.visibility = "visible"
     startButton.style.visibility = "hidden"
     matchButton.style.visibility = "hidden"
     document.getElementById("userName").innerHTML = (user.userName)
     for(let i = 0; i < 10; i++){
         rateBox = document.createElement('div')
+        rateBox.innerHTML = i+1
         rateDisplay.appendChild(rateBox)
         for (let j = 0; j < 5; j++){
             rate = document.createElement('button')
+            // rate.classList.add("button")
+            // rate.classList.add("is-info")
+            rate.classList.add("rateButton")
             rate.innerHTML = j+1
             rate.id = "r"+i+"c"+j
-            rate.addEventListener("click", registerScore, false)
+            rate.addEventListener("click", rateClickEvent)
             rateBox.appendChild(rate)
             rates.push(rate)
         } 
@@ -44,8 +62,12 @@ function initialte() {
     }
     let nextButton = document.createElement('button')
     nextButton.innerHTML = "Next"
-    rateDisplay.appendChild(nextButton)
-    nextButton.addEventListener("click", continuePlay, true)
+    nextButton.classList.add("button")
+    nextButton.classList.add("is-success")
+    nextButton.classList.add("is-fullwidth")
+    nextButton.style.marginBottom="5%"
+    submitDisplay.appendChild(nextButton)
+    nextButton.addEventListener("click", continuePlay)
     nextButton.id = "next"
 }
 startButton.onclick = initialte
@@ -54,70 +76,117 @@ function generateQuestions(pageNum){
     if (currentPage == 0){
         for (let i = 0; i < 10; i++){
             questionBlock = document.createElement('div')
+            questionBlock.classList.add("questions")
             // questionBlock.id = ("ques" + i)
-            questionBlock.innerHTML = questions[pageNum*10+i].question
+            questionBlock.innerHTML = i+1 + " " + questions[pageNum*10+i].question
             questionDisplay.appendChild(questionBlock)
             blocks.push(questionBlock)
         }
     } else {
         for (let i = 0; i < 10; i++){
-            blocks[i].innerHTML = questions[pageNum*10+i].question
+            blocks[i].innerHTML = i+1 + " " + questions[pageNum*10+i].question
         }
     }
 }
 
-function registerScore(e){
+function rateClickEvent(e){
     let target = e.target
     let score = target.innerHTML
     let id = target.id
 
     e.target.style.backgroundColor = "red"
-    document.getElementById("test").innerHTML = ("今天哪位小宝贝被点到了呢：" + id + "---" + score)
 
     let rId = parseInt(id[1])
     let cId = parseInt(id[3])
+    clicked[rId*5 + cId] = true
     for (let i = 0; i < 5; i++){
-        if(clicked[rId*5+i] == true && rId*5+i != id){
+        if(clicked[rId*5+i] == true && rId*5+i != rId*5+cId){
             // console.log(rates[i])
-            rates[rId*5+i].style.backgroundColor = "blue"
+            rates[rId*5+i].style.backgroundColor = "#008CBA"
+            clicked[rId*5+i] = false
+            //console.log(clicked)
         }
     }
-    clicked[rId*5 + cId] = true
+    
     scores[rId] = parseInt(score)
 }
 
+
 function continuePlay(){
-    let roundScore = scores.reduce(function (sum, score) {
-        return sum + score;
-      }, 0);
-      finalScore = finalScore + roundScore
+    // register final score
+    registerScore()
+    
     resetBoard()
+    // console.log(scores)
     currentPage = currentPage+1
     document.getElementById("progress").innerHTML = currentPage*(20) + '%'
     if(currentPage == 4){
         document.getElementById("next").innerHTML = "Finish"
         generateQuestions(currentPage)
-        document.getElementById("next").removeEventListener("click", continuePlay, true)
-        document.getElementById("next").addEventListener("click", endGame, false)
+        document.getElementById("next").removeEventListener("click", continuePlay)
+        document.getElementById("next").addEventListener("click", endGame)
     } else {
         generateQuestions(currentPage)
     }
 }
 
+function registerScore(){
+    finalScore.extraversion += scores[0]
+    finalScore.agreeableness -= scores[1]
+    finalScore.conscientiousness += scores[2]
+    finalScore.emotional_stability-= scores[3]
+    finalScore.intellect += scores[4]
+    finalScore.extraversion -= scores[5]
+    finalScore.agreeableness += scores[6]
+    finalScore.conscientiousness -= scores[7]
+    finalScore.emotional_stability += scores[8]
+    finalScore.intellect -= scores[9]
+}
+
 function resetBoard(){
-    rates.forEach((rate)=>rate.style.backgroundColor = "blue")
-    scores.forEach((score)=>score = 0)
-    rates.forEach((rate) => rate.addEventListener("click", registerScore, false))
+    rates.map((rate)=>rate.style.backgroundColor = "#008CBA")
+    scores = scores.map((score)=> score = 0)
+    clicked = clicked.map((c)=> c = false)
+
+    // console.log("reset")
 }
 
 function endGame(){
-   console.log("you have fucking finished what do you want?")
-   let roundScore = scores.reduce(function (sum, score) {
-    return sum + score;
-  }, 0);
-  finalScore = finalScore + roundScore
-    console.log("score: " + finalScore)
-   
+    document.getElementById("progress").innerHTML = "100%"
+    // register final score
+    registerScore()
+
+    document.getElementById("gameDisplay").remove()
+
+    let ScoreDisplay = document.createElement('div')
+    let extraversionDisplay = document.createElement('div')
+    extraversionDisplay.innerHTML = "Extraversion: " + finalScore.extraversion
+    let agreeablenessDisplay = document.createElement('div')
+    agreeablenessDisplay.innerHTML = "Agreeableness: " + finalScore.agreeableness
+    let conscientiousnessDisplay = document.createElement('div')
+    conscientiousnessDisplay.innerHTML = "Conscientiousness: " + finalScore.conscientiousness
+    let emotional_stabilityDisplay = document.createElement('div')
+    emotional_stabilityDisplay.innerHTML = "Emotional stability: " + finalScore.emotional_stability
+    let intellectDisplay = document.createElement('div')
+    intellectDisplay.innerHTML = "Intellect: " + finalScore.intellect
+
+    ScoreDisplay.appendChild(extraversionDisplay)
+    ScoreDisplay.appendChild(agreeablenessDisplay)
+    ScoreDisplay.appendChild(conscientiousnessDisplay)
+    ScoreDisplay.appendChild(emotional_stabilityDisplay)
+    ScoreDisplay.appendChild(intellectDisplay)
+
+    ScoreDisplay.classList.add("center")
+    body.appendChild(ScoreDisplay)
+    ScoreDisplay.style.marginBottom = "3%"
+
+    document.getElementById("next").innerHTML = "Find Your Matches"
+    document.getElementById("next").removeEventListener("click", endGame)
+    document.getElementById("next").addEventListener("click", findMatches)
+}
+
+function findMatches(){
+    alert("Find your Matches")
 }
 
 
