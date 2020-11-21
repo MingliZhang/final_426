e9cf74a4a9fa7a3f370717dfd744c0b768a2205e
hhp <haopenghuang0@gmail.com>
2020-11-20 22:24:56 -0500

    cookie done

--- main_interface/main.js
+++ main_interface/main.js
@@ -1,19 +1,20 @@
+var socket = undefined;
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();  
   
 });
 
-let socket = io();
-socket.on('message', message=>{
-  console.log(message)
-})
+function buildConnection(){
+  socket = io();
+  socket.on('message', message=>{
+    console.log(message)
+  })
 
-socket.on('chatMessage', message=>{
-  render(message);
-})
+  socket.on('chatMessage', message=>{
+    renderOthers(message);
+  })
 
-
-
+}
 
 
 $('.textarea').keydown(function (e) {
@@ -21,8 +22,7 @@ $('.textarea').keydown(function (e) {
   if ((event.keyCode == 10 || event.keyCode == 13) && event.ctrlKey){
     const value = event.target.value;
     socket.emit('chatMessage', value);
-    
-    
+    render(value);
     event.target.value='';
     event.target.focus();
   }
@@ -30,20 +30,29 @@ $('.textarea').keydown(function (e) {
 
 function showChat(){
   var temp = $('#chatWindow');
-  temp.css('visibility', 'visible')
+  temp.css('visibility', 'visible');
+  buildConnection();
   let chat_ul = document.getElementById('chat-ul')
   //need to load history
   chat_ul.scrollTop = chat_ul.scrollHeight;
 }
 
 function leave(){
+  socket.disconnect();
   $('#chatWindow').css('visibility', 'hidden')
+  socket = undefined;
 }
 
 
 
 function render(msg){
   $('#chat-ul').append(`<li class = "me">${msg}</li>`);
+  let chat_ul = document.getElementById('chat-ul')
+  chat_ul.scrollTop = chat_ul.scrollHeight;
+}
+
+function renderOthers(msg){
+  $('#chat-ul').append(`<li class = "him">${msg}</li>`);
   let chat_ul = document.getElementById('chat-ul')
   chat_ul.scrollTop = chat_ul.scrollHeight;
 }
