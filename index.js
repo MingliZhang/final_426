14ddf327fc58098d36ac287e57922e1d401a63b6
hhp <haopenghuang0@gmail.com>
2020-11-20 23:06:00 -0500

    make it better

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-// const express = require('express');
-// const http = require('http');
-// const socketio = require('socket.io');
-// const path = require('path');
-// const bodyParser = require('body-parser');
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 
-// const app = express();
+const app = express();
 
 
-// app.use(bodyParser.json())
-// app.use(express.static(__dirname))
-// const server = http.createServer(app);
-// const io = socketio(server);
+app.use(bodyParser.json())
+app.use(express.static(__dirname))
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, ()=>{
+    console.log("Working on port " + PORT);
+});
+const io = socketio(server);
 
 
-// io.on('connection', socket=>{
-//     socket.emit('message', 'welcome');
-//     socket.broadcast.emit('message', 'A user has joined the chat')
+io.on('connection', socket=>{
+    socket.emit('message', 'welcome');
+    socket.broadcast.emit('message', 'A user has joined the chat')
     
-//     socket.on('disconnect', ()=>{
-//         socket.broadcast.emit('message', 'the user has left the chat')
-//     })
+    socket.on('disconnect', ()=>{
+        socket.broadcast.emit('message', 'the user has left the chat')
+    })
 
-//     socket.on('chatMessage', msg=>{
-//         socket.broadcast.emit('chatMessage', msg)
-//     });
-// })
-
-
-
-// const PORT = process.env.PORT || 3000;
-// server.listen(PORT, ()=>{
-//     console.log("Working on port " + PORT);
-// });
+    socket.on('chatMessage', msg=>{
+        socket.broadcast.emit('chatMessage', msg)
+    });
+})
