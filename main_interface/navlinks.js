ae35b2fc63b940f01b16a2c03054291f3ebe95ec
Eva Hu <huyinuo@live.unc.edu>
2020-11-22 18:23:54 +0800

    Update mybox

--- main_interface/navlinks.js
+++ main_interface/navlinks.js
@@ -1,8 +1,11 @@
 $(function(){
     $('#homeicon a').attr("href", "main_interface.html")
     $('#gameicon a').attr("href", "../game/index.html")
-    $('#inboxicon a').attr("href", "../postbox/index.html")
-    $('#chaticon a').attr("href", "#")
+    $('#inboxicon a').attr("href", "../postbox/mybox.html")
+    $('#chaticon a').attr("href", "../postbox/index.html")
     $('#questionicon a').attr("href", "../questionnaire/index.html")
     $('#usericon a').attr("href", "#")
+    $(".icons a").css({ "font-size": "2.5vw", "color": "#0c3327" });
+    $(".icons span").css({ "font-size": "1.5vw", "color": "#0c3327", "padding-left": "1vw" });
+    $("#navbar").css({ "padding-left": "0", "padding-right": "1vw" });
 })
--- navbar/nav.js
+++ navbar/nav.js
@@ -3,7 +3,8 @@ $(function () {
   $("body").css("display", "flex");
   $("#navbar").css({ "padding-left": "1vw", "padding-right": "1vw" });
   $("#topicons").css("margin-top", "30%");
-  $(".icons a").css({ "font-size": "3vw", color: "#0c3327" });
+  $(".icons a").css({ "font-size": "3vw", "color": "#0c3327" });
+  $(".icons span").css({ "font-size": "2vw", "color": "#f3ECDB", "padding-left": "1vw" });
   $("#usericon").css("margin-top", "60%");
   // loadContent();
 });
@@ -11,16 +12,16 @@ $(function () {
 const loadNavIntoDom = function () {
   const $navbar = $(`#navbar`);
   let navbar = `
-    <div class = "column">
+    <div class = "column" style = "width: 20vw;">
         <div id = "topicons">
-            <div class = "icons" id = "homeicon"><a href = "#"><i class="fas fa-home"></i><span> Home</span></a></div>
-            <div class = "icons" id = "gameicon"><a href = "#"><i class="fas fa-gamepad"></i><span> Game</span></a></div>
-            <div class = "icons" id = "inboxicon"><a href = "#"><i class="fas fa-envelope-open-text"></i><span> inbox</span></a></div>
-            <div class = "icons" id = "chaticon"><a href = "#"><i class="fas fa-comments"></i><span> chat</span></a></div>
-            <div class = "icons" id = "questionicon"><a href = "#"><i class="far fa-map"></i></i></a></div>
+            <div class = "icons" id = "homeicon"><a href = "#"><i class="fas fa-home"></i><span>Home</span></a></div>
+            <div class = "icons" id = "gameicon"><a href = "#"><i class="fas fa-gamepad"></i><span>Game</span></a></div>
+            <div class = "icons" id = "inboxicon"><a href = "#"><i class="fas fa-envelope-open-text"></i><span>Inbox</span></a></div>
+            <div class = "icons" id = "chaticon"><a href = "#"><i class="fas fa-box-open"></i><span>Post Box</span></a></div>
+            <div class = "icons" id = "questionicon"><a href = "#"><i class="far fa-map"></i><span>Questionnaire</span></a></div>
         </div>
         <div class = "icons" id = "usericon">
-            <a href = "#"><i class="fas fa-user-circle"></i></a>
+            <a href = "#"><i class="fas fa-user-circle"></i><span>User</span></a>
         </div>
     </div>`;
 
