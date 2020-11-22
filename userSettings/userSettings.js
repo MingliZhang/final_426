af77fa1676b0cb3a491c2b39d8d7a726e3e49b18
lkh9908 <46586250+lkh9908@users.noreply.github.com>
2020-11-22 13:30:23 -0500

    change password

--- userSettings/userSettings.js
+++ userSettings/userSettings.js
@@ -1,26 +1,44 @@
-import {getUser} from '../postbox/getUser.js'
 
+import {getUser} from '../postbox/getUser.js'
+let user
 $(async function () {
-    let user = await getUser();
+    user = await getUser();
+    let userId = user.id
     let userName = user.userName;
     let userEmail = user.email
     let userMatchPoint = user.matchPoint
     let userGameScore = user.highestGameScore
-    console.log(user)
+
     $('#title').append(`<h2 class = contents>Username: ${userName}</h2>`)
     $('#title').append(`<h2 class = contents>Email: ${userEmail}</h2>`)
+    
     $('#title').append(`<h2 class = contents>Your Personality Score: </h2>`)
     $('#title').append(`<h2 class = contents>
-        extraversion: ${userMatchPoint[0]}
-        agreeableness: ${userMatchPoint[0]}
-        conscientiousness: ${userMatchPoint[0]}
-        emotional_stability: ${userMatchPoint[0]}
-        intellect: ${userMatchPoint[0]}
+        Extraversion: ${userMatchPoint[0]} --
+        Agreeableness: ${userMatchPoint[0]} --
+        Conscientiousness: ${userMatchPoint[0]} --
+        Emotional_stability: ${userMatchPoint[0]} --
+        Intellect: ${userMatchPoint[0]}
         </h2>`)
     $('#title').append(`<h2 class = contents>Your Highest Game Point: ${userGameScore}</h2>`)
-
-    $('#root').append(`<button type = "button" class = "fas fa-sign-out-alt button  is-danger" id = "logoutButton"> &nbsp Log Out</button>`)
     $('#logoutButton').on('click', handleLogOut);
+
+    document.getElementById('changePassword').addEventListener('click', async () => {
+        var password = prompt("Please Enter Your New Password", "");
+        if (password != null && password != "") {
+            user.password = password
+            try{
+                const result = await axios({
+                    method: 'put',
+                    url: `https://us-central1-comp426-firebase.cloudfunctions.net/users/${userId}`,
+                    data: user
+                });     
+            } catch{
+                console.error('put following error')
+            }
+        }
+        confirm("Your Password has been changed!") 
+    })
 });
 
 const handleLogOut = function(){
